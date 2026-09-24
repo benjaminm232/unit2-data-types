08ed15a9b492803dd7412d822e5c9c482d2819fa
benjaminm232 <benjaminm232@nycstudents.net>
2026-09-24 05:46:47 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+bill = input()
+print(bill)
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+def tip_calculator(a)
+    a = int(input("How much was your bill?"))
+    tip_value = 0
+    z = input("How was your service?")
