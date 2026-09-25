bf3a24b7598ac8fdc3e1bbeb8221f910dc7d024e
benjaminm232 <benjaminm232@nycstudents.net>
2026-09-25 06:24:40 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,18 @@
-bill = input()
-print(bill)
+""" bill = input()
+print(bill) """
 
 """ values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
     print(i) """
 
-def tip_calculator(b):
-    b = input("How much was your bill?")
-    # tip_value = 0
-    # z = input("How was your service?")
-print (b)
+""" bill = float(input("How much was your bill?: $"))
+tip_percent = float(input("What percentage tip would you like to give?: "))
+tip_amount = bill * (tip_percent/100)
+total_bill = bill + tip_amount 
+print(total_bill)
+ """
+
+print("Welcome to tip calculator")
+bill = float(input("How much was your bill? "))
+service = input("How was your service: Bad, Okay, Good, Great? ")
